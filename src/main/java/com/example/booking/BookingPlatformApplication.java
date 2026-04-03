ca6d68b30c57554066204ad57a74eda498fa5dbc
Shridhara_R <43702120+wargod797@users.noreply.github.com>
2026-04-03 12:56:51 +0530

    initial commit

---
+++ src/main/java/com/example/booking/BookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/booking/BookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
